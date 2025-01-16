700a7ab518769ee11ac58482c4fd47aacb2454a1
Marttify <aguilarmartin977@gmail.com>
2025-01-16 18:37:44 -0300

    fix(env): asegurar el archivo de conexion con Firebase usando las variables de entornos

--- apps/backend/config/firebaseConfig.ts
+++ apps/backend/config/firebaseConfig.ts
@@ -1,6 +1,25 @@
 
-import * as admin from 'firebase-admin';  
-import * as serviceAccount from './api-mascotas-46740-firebase-adminsdk-tpf97-2ab385f558.json'; // Ajusta la ruta según tu estructura  
+import * as admin from 'firebase-admin';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+};
+
+if (!serviceAccount.project_id) {
+  throw new Error('Faltan datos del proyecto Firebase. Compruebe sus variables de entorno.');
+}
 
 admin.initializeApp({  
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount), // Configura las credenciales  
